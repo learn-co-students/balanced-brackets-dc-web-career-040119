ef67abfa4c45a7b523b0a7ba14b14a3a8212878a
github username <github email address>
2019-07-16 15:07:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// const isBalanced = (str) => {
-//     let allBalanced = true
-//     const opening = [/{/, /\[/, /\(/]
-//     const closing = [/}/, /\]/, /\)/]
-//
-//     opening.forEach((bracket, i) => {
-//         console.log(str)
-//         debugger
-//         if (bracket.test(str)) {
-//             if (closing[i].test(str)) {
-//                 const openingIndex = bracket.exec(str).index
-//                 const closingIndex = closing[i].exec(str).index
-//                 if (openingIndex < closingIndex) {
-//                     if (closingIndex - openingIndex === 1) {
-//                         allBalanced = isBalanced(str.slice(0, openingIndex) + str.slice(closingIndex + 1))
-//                     } else if (isBalanced(str.slice(openingIndex + 1, closingIndex))) {
-//                         allBalanced = isBalanced(str.slice(closingIndex + 1))
-//                     } else {
-//                         return allBalanced = isBalanced(str.slice)
-//                     }
-//                 } else {
-//                     return allBalanced = false
-//                 }
-//             } else {
-//                 return allBalanced = false
-//             }
-//         } else if (closing[i].test(str)) {
-//             return allBalanced = false
-//         }
-//     })
-//     return allBalanced
-// }
-
 const isBalanced = (str) => {
     const brackets = [
          /\{\}/g,
